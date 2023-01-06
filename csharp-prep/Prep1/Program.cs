376394044081fe0a0d5425fa61c2b352854f367c
Cameron Finch <camfynch@gmail.com>
2023-01-06 11:37:30 -0700

    commited

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is Your First Name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is Your Last Name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your Name Is {lastName}, {firstName}{lastName} ");
     }
 }

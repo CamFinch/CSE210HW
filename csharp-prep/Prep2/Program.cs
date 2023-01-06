95ffd0895a2eb0f7b5d173a809a4cccfdeb94c6f
Cameron Finch <camfynch@gmail.com>
2023-01-06 12:49:09 -0700

    commited

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Grade Percentage: ");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+
+        string grade = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your Letter Grade Is A: {grade}");
+        
+        
     }
 }
+

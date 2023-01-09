b46b7cb70ec1a80f211fe7aa4a407b896b915366
Cameron Finch <camfynch@gmail.com>
2023-01-09 15:21:46 -0700

    commited

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string inputName = Name();
+        int inputNum = InputNumber();
+
+        int squared = squareRoot(inputNum);
+
+        Information(inputName, inputNum);
+    }
+
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome!");
+    }
+    static string Name(){
+
+        Console.WriteLine("What is Your Name: ");
+        string name = Console.ReadLine();
+
+        return name;
+
+    }
+
+    static int InputNumber(){
+        Console.WriteLine("Please Enter A Number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        return num;
+    }
+
+    static int squareRoot(int num){
+        int square = num * num;
+        return square;
+    }
+
+    static void Information(string name, int square){
+        Console.WriteLine($"Your Name {name}, Square Root Of Number Given: {square}");
     }
 }

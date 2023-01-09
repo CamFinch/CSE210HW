5dc46b6222701fd311f27e69e062b1151f6e3d1e
Cameron Finch <camfynch@gmail.com>
2023-01-09 12:26:23 -0700

    commited

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNum = new Random();
+        int num = randomNum.Next(1, 100);
+
+        int i = -1;
+
+        while (i != num){
+            Console.WriteLine("Guess A Number Between 1-100: ");
+            i = int.Parse(Console.ReadLine());
+
+            if (num < i){
+                Console.WriteLine("Lower");
+
+            }
+            else if (num > i){
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("Correct!");
+            }
+
+        }
+        
+        
+
+
     }
 }

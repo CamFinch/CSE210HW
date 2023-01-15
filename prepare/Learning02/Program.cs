391e60b1be0cf407d9c600d88bd7c6914a64afee
Cameron Finch <camfynch@gmail.com>
2023-01-15 16:20:01 -0700

    committed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        Resume resume1 = new Resume();
+        job1._jobTitle = "Teacher";
+        job1._company = "BYUI";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Doctor";
+        job2._company = "Hospital";
+        job2._startYear = 2019;
+        job2._endYear = 2023;
+
+        resume1._name = "Cameron Finch";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
-}
+} 

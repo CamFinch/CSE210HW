9a77c10d7c4c109b8c209b251d1b8484a8b7efc7
Cameron Finch <cameronfinch@Camerons-Air.hsd1.nm.comcast.net>
2023-01-28 20:33:10 -0700

    commited

--- prove/Develop02/PromptGen.cs
+++ prove/Develop02/PromptGen.cs
@@ -1,28 +1,40 @@
 using System;
 using System.IO;
 
-public class PromptGen
+class PromptGen
 {
 
     public string prompt;
-    
+
+
     public void GivePrompt(){
-
-        List<string> promptOption = new List<string>{
-            "Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?",
-            "What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?"
-        };
-
-        foreach(string line in promptOption){
-             string[] parts = line.Split(",");
-
-            string prompt = parts[1];
-            
+        Random num = new Random();
+        int randomnumber = num.Next(0, 5);
+        
+            //"Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?",
+            //"What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?"
+        if(randomnumber == 1){
+            prompt = "Who was the most interesting person I interacted with today?";
+        }
+        else if(randomnumber == 2){
+            prompt = "What was the best part of my day?";
+        }
+        else if(randomnumber == 3){
+            prompt = "How did I see the hand of the Lord in my life today?";
+        }
+        else if(randomnumber == 4){
+            prompt = "What was the strongest emotion I felt today";
+        }
+        else if(randomnumber == 5){
+            prompt = "If I had one thing I could do over today, what would it be?";
         }
 
         Console.WriteLine(prompt);
+
+        
         
 
     }
 
+    
 }

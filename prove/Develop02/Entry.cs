303903b638514d957dc38334f6b700479d8b6db1
Cameron Finch <cameronfinch@Camerons-Air.hsd1.nm.comcast.net>
2023-02-06 17:56:18 -0700

    commited

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -4,55 +4,16 @@ using System.IO;
 
 public class Entry
 {
-    public string filePath;
-    public string journalEntry;
+    public DateTime _date = DateTime.Now;   
+    public string _prompt;
+    public string _response;
+    //public string AddedEntry(){
+        //string added = $"{_date} \n{_prompt}: \n{_response}";
+        //return added;
+    //}
 
-    public string newFileName;
-
-    public string fileName;
-    
-    
-
-
-    DateTime date = DateTime.Now;
-   
-
-    
-    public void EntryAndSave(){
-
-        Console.WriteLine("What Would you Like to name the file to be saved to?");
-        string newFileName = Console.ReadLine();
-
-        
-
-        Console.WriteLine($"Prompt: ");
-        string journalEntry = Console.ReadLine();
-
-        using (StreamWriter outputFile = new StreamWriter(newFileName)){
-            outputFile.WriteLine($"{date} \nPrompt: \n{journalEntry}");
-
-       
-
-
-    }
-    }
-
-    public void Load(){
-
-    Console.WriteLine("Enter File To Load From: ");
-    string fileName = Console.ReadLine();
-
-
-    
-    List<string> lines = System.IO.File.ReadAllLines(fileName).ToList();
-
-    foreach(string s in lines)
-{
-    Console.WriteLine(s);
-}  
-    
-
-
+    public void DisplayEntry(){
+        Console.WriteLine($"{_date} \nPrompt: {_prompt} \n{_response}");
     }
     
 }

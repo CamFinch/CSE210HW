1d793db1507cb0efd93d12893400c9b232888ca4
Cameron Finch <cameronfinch@Camerons-Air.hsd1.nm.comcast.net>
2023-02-08 15:06:24 -0700

    commited

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+
+public class Word{
+    public int randomNumber;
+    public string joined;
+    
+    
+    //Trust", "in", "the", "Lord", "with", "all", "thine", "heart;", "and", "lean", "not", "unto", "thine", "own", "understanding."
+    List<string> shownText = new List<string>{"Trust", "in", "the", "Lord", "with", "all", "thine", "heart;", "and", "lean", "not", "unto", "thine", "own", "understanding."};
+
+
+    public void Hide(){
+        
+
+        Random random = new Random();
+        int randomNumber = random.Next(0, shownText.Count);
+        shownText.RemoveAt(randomNumber);
+        shownText.Insert(randomNumber, "---");
+        
+    }
+
+    public void WriteText(){
+        string joined = string.Join(" ", shownText);
+        Console.WriteLine(joined);
+        
+    }
+
+    
+    
+}

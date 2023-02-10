408f69f8f9e40e99d941f4820d9f6ac25510b633
Cameron Finch <cameronfinch@Camerons-Air.hsd1.nm.comcast.net>
2023-02-09 22:18:26 -0700

    commited

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -4,8 +4,8 @@ using System.IO;
 public class Word{
     public int randomNumber;
     public string joined;
-    
-    
+    // 5        2     3      4      4       3       5        6       3         4      3       4      5        3        14
+    // 0       1     2       3      4       5       6       7          8       9      10     11     12        13      14
     //Trust", "in", "the", "Lord", "with", "all", "thine", "heart;", "and", "lean", "not", "unto", "thine", "own", "understanding."
     List<string> shownText = new List<string>{"Trust", "in", "the", "Lord", "with", "all", "thine", "heart;", "and", "lean", "not", "unto", "thine", "own", "understanding."};
 
@@ -16,58 +16,50 @@ public class Word{
         int randomNumber = random.Next(0, shownText.Count);
         int randomNumber2 = random2.Next(0, shownText.Count);
         int numLetter = shownText[randomNumber].Length;
-        if(numLetter == 3){
-            if(shownText[randomNumber] != "---"){
+        if(shownText[randomNumber] != "--" || shownText[randomNumber] != "---" || shownText[randomNumber] != "----" || shownText[randomNumber] != "-----" || shownText[randomNumber] != "------" || shownText[randomNumber] != "--------------" ){
+            if(numLetter == 2){
+  
+                shownText.RemoveAt(randomNumber);
+                shownText.Insert(randomNumber, "--");
+            }
+            else if(numLetter == 3){
+ 
                 shownText.RemoveAt(randomNumber);
                 shownText.Insert(randomNumber, "---");
-                shownText.RemoveAt(randomNumber2);
-                shownText.Insert(randomNumber2, "---");
+
             }
-            else{
-                Hide();
-            }
-        }
-        if(numLetter == 4){
-            if(shownText[randomNumber] != "----"){
+            else if(numLetter == 4){
                 shownText.RemoveAt(randomNumber);
                 shownText.Insert(randomNumber, "----");
-                shownText.RemoveAt(randomNumber2);
-                shownText.Insert(randomNumber2, "----");
+        
             }
-            else{
-                Hide();
-            }
-        }
-        if(numLetter == 5){
-            if(shownText[randomNumber] != "-----"){
+            else if(numLetter == 5){
                 shownText.RemoveAt(randomNumber);
                 shownText.Insert(randomNumber, "-----");
-                shownText.RemoveAt(randomNumber2);
-                shownText.Insert(randomNumber2, "-----");
+           
             }
-            else{
-                Hide();
-            }
-        }
-        if(numLetter == 6){
-            if(shownText[randomNumber] != "------"){
+            else if(numLetter == 6){
+            
                 shownText.RemoveAt(randomNumber);
-                shownText.Insert(randomNumber, "------");
-                shownText.RemoveAt(randomNumber2);
-                shownText.Insert(randomNumber2, "------");
+                shownText.Insert(randomNumber, "------");  
             }
-            else{
-                Hide();
+            else if(numLetter == 14){
+            
+                shownText.RemoveAt(randomNumber);
+                shownText.Insert(randomNumber, "-------------");
+
             }
         }
-
+        else{
+            Hide();
+        }
         
     }
 
     public void WriteText(){
         string joined = string.Join(" ", shownText);
         Console.WriteLine(joined);
-        if(shownText[0] == "---" && shownText[1] == "---" && shownText[2] == "---" && shownText[3] == "---" && shownText[4] == "---" && shownText[5] == "---" && shownText[6] == "---" && shownText[7] == "---" && shownText[8] == "---" && shownText[9] == "---" && shownText[10] == "---" && shownText[11] == "---" && shownText[12] == "---" && shownText[13] == "---" && shownText[14] == "---" ){
+        if(shownText[0] == "-----" && shownText[1] == "--" && shownText[2] == "---" && shownText[3] == "----" && shownText[4] == "----" && shownText[5] == "---" && shownText[6] == "-----" && shownText[7] == "------" && shownText[8] == "---" && shownText[9] == "----" && shownText[10] == "---" && shownText[11] == "----" && shownText[12] == "-----" && shownText[13] == "---" && shownText[14] == "-------------" ){
             System.Environment.Exit(0);
         }
     }

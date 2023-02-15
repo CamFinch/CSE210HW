ed673ad45c5a63514b0b67589bcf345743491873
Cameron Finch <cameronfinch@Camerons-Air.hsd1.nm.comcast.net>
2023-02-15 10:53:49 -0700

    commited: git commit -m commited

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Cameron Finch", "Math");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Cameron Finch", "Addition", "1.3", "1-5");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Cameron Finch", "World History", "The Mongol Empire");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritinInfo());
     }
 }

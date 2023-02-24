07264f40a76bdb2db47753d30536ce3f53ea048b
Cameron Finch <cameronfinch@Camerons-Air.hsd1.nm.comcast.net>
2023-02-24 00:56:56 -0700

    commited

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.IO;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool menuActive = true;
+        Control control1 = new Control("Menu", "End");
+        while(menuActive){
+            Console.Write("Welcome!");
+            Console.Write("\nMenu Options: \n");
+            Console.WriteLine("\n1. Breathing Activity \n2. Reflecting Activity \n3. Listing Activity \n4. Quit");
+            Console.WriteLine("\nSelect Option: ");
+            string choice = Console.ReadLine();
+
+            if(choice == "1"){
+                Breathing breathing1 = new Breathing("Breathing Activity", "This activity will help you control your breathing.", "\nThanks for participating!");
+                breathing1.BreathingStart();
+            }
+            else if(choice == "2"){
+                
+                Reflection reflection1 = new Reflection("Refelcting Activity", "This activity will help you reflect.", "\nThanks for participating!");
+                reflection1.ReflectionStart();
+
+            }
+            else if(choice == "3"){
+
+                Listing listing1 = new Listing("Listing Activity", "This activity allows you to list and ponder.", "\nThanks for participating!");
+                listing1.ListingStart();
+                
+
+            }
+            else if(choice == "4"){
+                menuActive = false;
+
+            }
+            else{
+                Console.WriteLine("Incorrect Input Please Try Again!");
+                
+            }
+        }
+        
     }
 }

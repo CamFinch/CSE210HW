008070fb63f397327c3a78fc3446ec955b77cce6
Cameron Finch <cameronfinch@Camerons-MacBook-Air.local>
2023-03-18 15:51:26 -0600

    commited

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        
+        Customer c1 = new Customer("Cameron Finch");
+        Address a1 = new Address("538 S Second W", "Rexburg", "ID", "USA");
+        Product p1 = new Product("apple", 345345, 3, 1);
+        Product p2 = new Product("phone", 324, 3, 400);
+        Order o1 = new Order();
+        Console.WriteLine($"{o1.PackingLabel}");
+        Console.WriteLine($"{o1.ShippingLabel}");
+
     }
 }

7ed7a211a3dc3ccf7633d43c2318b82f85150f11
Cameron Finch <cameronfinch@Camerons-MacBook-Air.local>
2023-03-18 15:49:14 -0600

    commited

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> acvitivies = new List<Activity>();
+
+        //string date, int time, int distance
+
+        Running r1 = new Running("March 10, 2023", 45, 3);
+        acvitivies.Add(r1);
+        
+        //string date, int time, int speed
+
+        Cycling c1 = new Cycling("April 9, 2023", 60, 10);
+        acvitivies.Add(c1);
+
+        //string date, int time, int laps
+
+        Swimming s1 = new Swimming("April 18, 2023", 30, 35);
+        acvitivies.Add(s1);
+
+        foreach(Activity act in acvitivies){
+            string dates = act.GetDate();
+            int times = act.GetTime();
+            string sum = act.GetSummary();
+            Console.WriteLine($"{dates} Running ({times} Minutes) {sum}");
+        }
     }
 }
